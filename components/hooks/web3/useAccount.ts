da2b6115424d936d6939efdfe73c639b6b684f5d
swapnil <swapnilkole7500@gmail.com>
2022-08-03 16:05:47 +0530

    account hook completed

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,19 +1,68 @@
 import { CryptoHookFactory } from "@_types/hooks";
+import { useEffect } from "react";
 import useSWR from "swr";
 
-
-type AccountHookFactory = CryptoHookFactory<string, string> 
+type useAccountResponse = {
+    connect: () => void,
+    isLoading: boolean,
+    isInstalled: boolean,
+}
+type AccountHookFactory = CryptoHookFactory<string, useAccountResponse> 
 export type UseAccountHook = ReturnType<AccountHookFactory>
 
 
 //deps => provider, ethereum, contract (web3State)
-export const hookFactory: AccountHookFactory = (deps) => (params) => {
-    const swrRes = useSWR("web3/useAccount", async () => {
-        console.log(deps);
-        console.log(params);
-        return "Test User"
+export const hookFactory: AccountHookFactory = ({ provider, ethereum, isLoading }) => () => {
+    const { data, mutate, isValidating, ...swr } = useSWR(
+        provider ? "web3/useAccount" : null,
+        async () => {
+            const accounts = await provider!.listAccounts();
+            if (!accounts || accounts.length === 0) {
+                throw "Connect to wallet"
+            }
+            return accounts[0];
+        }, {
+            revalidateOnFocus: false,
+        }
+    )
+
+    useEffect(() => {
+        ethereum?.on("accountsChanged", handleAccountsChanged);
+        return () => {
+            ethereum?.removeListener("accountsChanged", handleAccountsChanged);
+        }
     })
 
-    return swrRes;
+    const handleAccountsChanged = (...args: unknown[]) => {
+        console.log("args", args);
+        const accounts = args[0] as string[];
+        if (accounts.length === 0) {
+            console.log("connect to wallet");
+        } else {
+            if (accounts[0] !== data) {
+                console.log("account changed", accounts[0]);
+                mutate(accounts[0]);
+            } 
+        }
+        console.log("acc changed")
+    }
+ 
+    const connect = async () => {
+        try {
+            ethereum?.request({ method: "eth_requestAccounts" });
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    return {
+        ...swr,
+        data,
+        isValidating,
+        isLoading: isLoading || isValidating,
+        isInstalled: ethereum?.isMetaMask || false,
+        mutate, 
+        connect
+    };
 }
 
--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -13,17 +13,26 @@ const Web3Provider: FunctionComponent<Children> = ({children}: any) => {
     const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
     useEffect(() => {
         async function initWeb3() {
-            const { ethereum } = window;
-            if (ethereum) {
-                const provider = new ethers.providers.Web3Provider(ethereum as any);
-                const contract = await loadContract("NftMarket",provider);
-                setWeb3Api(createWeb3State({
-                    ethereum,
-                    provider,
-                    contract,
-                    isLoading: false,
-                }));
+            try {
+                    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+                    const contract = await loadContract("NftMarket",provider);
+                    setWeb3Api(createWeb3State({
+                        ethereum: window.ethereum,
+                        provider,
+                        contract,
+                        isLoading: false,
+                    }));
             }
+            catch (e: any) {
+                console.error("Install web3 wallet");
+                setWeb3Api((api) => createWeb3State({
+                    ...api as any ,
+                    isLoading: false,
+                })
+
+                )
+            }
+            
         }
         initWeb3();
     }, [])
